2a9bc4e7ad9ff22ba17bcad8a73b17f8a11ac4c9
ccslakey <cromwellslakey@gmail.com>
2015-11-26 12:33:58 -0800

    basic clock down

--- js/clock.js
+++ js/clock.js
@@ -1,18 +1,50 @@
-$(function(){
-	var clockDiv = document.getElementById('clock');
-	
-	function displayTime(){
-		var now = new Date();
-		var hours = now.getHours();
-		var minutes = now.getMinutes();
-		var seconds = now.getSeconds();
+$(function() {
+var clockDiv = document.getElementById('clock');
+var dayDiv = document.getElementById('day-of-week');
+var dateMonthYearDiv = document.getElementById('date-month-year');
 
-		clockDiv.innerText = hours + ':' + minutes + ":" + seconds;
-
-	}
+    function displayTime() {
+        var now = new Date();        
+        var hours = now.getHours();
+        var minutes = now.getMinutes();
+        var seconds = now.getSeconds();
 
 
+        // 12-hour format
+        var meridiem = "AM";  // Default is AM
+        // Convert from 24 hour to 12 hour format
+        // and keep track of the meridiem.
+        if (hours > 12) {
+            hours = hours - 12;
+            meridiem = "PM";
+        }
 
+        // 0 AM and 0 PM should read as 12
+        if (hours === 0) {
+            hours = 12;    
+        }
+
+        // 1-9 becomes 01-09
+        if (seconds < 10) {seconds = "0"+seconds};
+        if (minutes < 10) {minutes = "0"+minutes};
+        if (hours < 10) {hours = "0"+hours};
+        clockDiv.innerText = hours + ':' + minutes + ":" + seconds + " " + meridiem;
+
+    }
+
+    function displayDate(){
+        var now = new Date();
+        var day = now.getDate();
+        var dayOfWeek = now.getDay();
+        var month = now.getMonth();
+        var year = now.getFullYear();
+
+        dayDiv.innerText = "hi what is going on"
+
+    }
+
+
+    setInterval(displayTime, 1000);
 
 
 })
